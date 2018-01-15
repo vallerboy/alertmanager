611dedacf421a3c263d497f54ee23ae24a6c7195
Oskar Polak <oskarpolak@vp.pl>
2018-01-15 18:53:19 +0100

    Initial commit

---
+++ src/main/java/pl/oskarpolak/alertmanager/AlertmanagerApplication.java
@@ -0,0 +1,11 @@
+package pl.oskarpolak.alertmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertmanagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AlertmanagerApplication.class, args);
+	}
+}
---
+++ src/main/java/pl/oskarpolak/alertmanager/controllers/MainController.java
@@ -0,0 +1,8 @@
+package pl.oskarpolak.alertmanager.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+}
