3bde0e83bd7194302fd4df6255505b6236990003
Oskar Polak <oskarpolak@vp.pl>
2018-01-16 20:54:33 +0100

    asd

--- src/main/java/pl/oskarpolak/alertmanager/models/AlertForm.java
+++ src/main/java/pl/oskarpolak/alertmanager/models/AlertForm.java
@@ -1,9 +1,18 @@
 package pl.oskarpolak.alertmanager.models;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class AlertForm {
-    @Size(min = 4, max = 50, message = "Alert nie może być krótszy niż 4 znaki i dłuższy niż 50")
+    @Size(min = 5, max = 6)
+    //@Min()
+    //@Max
+    @NotNull
+    //@Email
+    @Pattern(regexp = "[0-9]{2}-?[0-9]{3}")
     private String alert;
     private Boolean isWarning;
 
